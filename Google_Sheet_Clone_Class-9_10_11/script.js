f20c51590c3edd1317da934263ccc7565b6f5900
Neeraj Chaudhary <neerajchaudhary@Neerajs-Air.bbrouter>
2024-06-20 02:41:13 +0530

    Class 9 Google Sheet Clone 1

---
+++ Google_Sheet_Clone_Class-9_10_11/script.js
@@ -0,0 +1,42 @@
+const headRow = document.querySelector(".head-row");
+const headCol = document.querySelector(".serialNo");
+const body = document.querySelector(".body");
+const rows = 500,
+  columns = 27;
+
+(() => {
+  for (let i = 1; i < columns; i++) {
+    // create a cell;
+    const headCell = document.createElement("div");
+    // if (i === 0) {
+    //   //   headRow.appendChild(headCell);
+    //   continue;
+    // }
+    headCell.innerText = String.fromCharCode(64 + i);
+    headCell.classList.add("col-head");
+    headRow.appendChild(headCell);
+  }
+})();
+
+(() => {
+  for (let i = 0; i < rows; i++) {
+    const headColCell = document.createElement("div");
+    headColCell.innerHTML = i + 1;
+    headColCell.classList.add("sno-cell");
+    headCol.appendChild(headColCell);
+  }
+})();
+
+(() => {
+  for (let row = 1; row <= rows; row++) {
+    const rowCells = document.createElement("div");
+    rowCells.classList.add("row");
+    for (let col = 1; col < columns; col++) {
+      const colCell = document.createElement("cite");
+      colCell.classList.add("cell");
+      colCell.contentEditable = true;
+      rowCells.appendChild(colCell);
+    }
+    body.appendChild(rowCells);
+  }
+})();
